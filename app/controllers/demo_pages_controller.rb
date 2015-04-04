ad969934ed30dc9ce6557005847773fa918a925d
kashnuts <kashsuns@gmail.com>
2015-04-05 07:21:37 +0900

    Finish demo pages

---
+++ app/controllers/demo_pages_controller.rb
@@ -0,0 +1,10 @@
+class DemoPagesController < ApplicationController
+  def home
+  end
+
+  def team
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'demo_pages/home'
+
+  get 'demo_pages/team'
+
+  get 'demo_pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
